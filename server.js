43c9ffc9fd4df9f8f3aec6e132fcb3af74435284
klthemathnerd8 <91514631+klthemathnerd8@users.noreply.github.com>
2023-04-25 18:03:25 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+  socket.on('message', (message) => {
+    io.emit('message', message);
+  });
+});
